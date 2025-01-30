4954ea072a57efc7b997858fc1a05f2b897473b2
edalbye <162988817+edalbye@users.noreply.github.com>
2025-01-30 18:49:42 +0000

    Updated valid video file types
    Added a number of formats to list of accepted video file formats.

--- constants.py
+++ constants.py
@@ -1,1 +1,2 @@
-valid_video_file_types = {".mp4", ".mov"}
+#List of video file suffices that are compatible with ffmpeg
+valid_video_file_types = {".mp4", ".mov", ".webm", ".flv", ".ogv", ".ogg", ".avi", ".m4v", ".m4a"}
