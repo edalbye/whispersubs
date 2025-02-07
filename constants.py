db17a4ce92d21bfa1f30057477e8a7880ce0ec3e
edalbye <162988817+edalbye@users.noreply.github.com>
2025-02-07 11:04:01 +0000

    Updated list of supported languages
    Added all supported whisper languages to internal list of supported languages.

--- constants.py
+++ constants.py
@@ -2,7 +2,7 @@ from pathlib import Path
 #List of video file suffices that are compatible with ffmpeg
 valid_video_file_types = {".mp4", ".mov", ".webm", ".flv", ".ogv", ".ogg", ".avi", ".m4v", ".m4a"}
 supported_models = ["WhisperLargeV3", "WhisperLargeV3-Turbo"]
-supported_languages = ["English", "Spanish"]
+supported_languages = ["Afrikaans", "Arabic", "Armenian", "Azerbaijani", "Belarusian", "Bosnian", "Bulgarian", "Catalan", "Chinese", "Croatian", "Czech", "Danish", "Dutch", "English", "Estonian", "Finnish", "French", "Galician", "German", "Greek", "Hebrew", "Hindi", "Hungarian", "Icelandic", "Indonesian", "Italian", "Japanese", "Kannada", "Kazakh", "Korean", "Latvian", "Lithuanian", "Macedonian", "Malay", "Marathi", "Maori", "Nepali", "Norwegian", "Persian", "Polish", "Portuguese", "Romanian", "Russian", "Serbian", "Slovak", "Slovenian", "Spanish", "Swahili", "Swedish", "Tagalog", "Tamil", "Thai", "Turkish", "Ukrainian", "Urdu", "Vietnamese", "Welsh"]
 config_defaults = {
     'input_mode': "Single File",
     'filepath': str(Path.cwd()),
